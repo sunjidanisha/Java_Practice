539daf4c4664c9d7b13158cadaf0f4ec6c138681
sunjidanisha <saniharifa@gmail.com>
2024-08-27 20:08:22 +0600

    Day 12

--- src/test/java/java_basic/array/array2DElement.java
+++ src/test/java/java_basic/array/array2DElement.java
@@ -7,11 +7,12 @@ import java.util.Scanner;
 public class array2DElement {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        System.out.println("Enter a row and column number ");
         int rows = sc.nextInt();
         int cols = sc.nextInt();
 
         int[][] numbers = new int[rows][cols];
-
+        System.out.println("Create a matrix based on the row and column you have mentioned ");
         //input
         //rows
         for (int i = 0; i < rows; i++) {
@@ -20,13 +21,14 @@ public class array2DElement {
                 numbers[i][j] = sc.nextInt();
             }
         }
+        System.out.println("consider a number from the matrix as X to find the location");
         int x = sc.nextInt();
 
         // find x element
         for (int i = 0; i < rows; i++) {
             for (int j = 0; j < cols; j++) {
                 if (numbers[i][j] == x) {
-                    System.out.println("x found at location (" + i + ", " + j + ")");
+                    System.out.println("X found at location " + i + " , " + j );
 
                 }
 
