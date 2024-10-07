442d6ee9424ffabf1163c7e1cad32342ad260588
sunjidanisha <saniharifa@gmail.com>
2024-10-07 20:58:11 +0600

    changed

--- src/test/java/java_basic/Bit_manipulation/Task01.java
+++ src/test/java/java_basic/Bit_manipulation/Task01.java
@@ -7,7 +7,13 @@ import java.util.Scanner;
 public class Task01 {
     public static boolean isPowerOfTwo(int n){
         return  n > 0 && (n & (n - 1)) == 0;
-
+/* The key logic is the expression (n & (n - 1)) == 0.
+This checks if only one bit is set in the binary representation of n,
+which is true for powers of 2. The & operator performs a bitwise AND.
+Example: For n = 8 (which is 1000 in binary):
+    ->  n - 1 = 7 (which is 0111 in binary).
+    ->  8 & 7 results in 0000, so 8 is a power of 2.
+*/
     }
 
     public static void main(String[] args) {
